92cb2f5eb85b29a1c552d4ff5c829d7eb822e014
omersagi-2007 <omer.sagi.15@gmail.com>
2025-08-09 19:28:52 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// sw.js
+const CACHE_NAME = 'shooting-pwa-v4';
+const CORE_ASSETS = [
+  './',
+  './shooting.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => 
+      cached || fetch(req).then(res => {
+        if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(req, clone));
+        }
+        return res;
+      }).catch(() => cached)
+    )
+  );
+});
